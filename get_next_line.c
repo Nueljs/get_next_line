416738c7f23b42b84de1e8355cae166074642f14
Manuel Cervera <manuelcerveralara@gmail.com>
2025-12-15 06:09:12 +0100

    nuevo intento de hacer gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: macerver <macerver@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 05:20:05 by macerver          #+#    #+#             */
-/*   Updated: 2025/12/13 20:06:50 by macerver         ###   ########.fr       */
+/*   Updated: 2025/12/15 06:07:25 by macerver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static char	*fill_line(char *remember)
 	size_t	len;
 
 	i = 0;
-	if (!remember)
+	if (!remember || *remember == '\0')
 		return (NULL);
 	while (remember[i] && remember[i] != '\n')
 		i++;
@@ -41,33 +41,63 @@ static char	*handle_remember(char *remember, char *line)
 	return (remember);
 }
 
-char	*get_next_line(int fd)
+static char *fill_remember(char *remember, int fd)
 {
-	static char	*remember;
-	int			bytesread;
-	char		buffer[BUFFER_SIZE + 1];
-	char		*line;
-
+	char	buffer[BUFFER_SIZE + 1];
+	int		bytesread;
+	
 	bytesread = 1;
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	while (bytesread > 0)
+	while (!ft_strchr(remember,'\n'))
 	{
 		bytesread = read(fd, buffer, BUFFER_SIZE);
 		if (bytesread < 0)
 			return (NULL);
 		buffer[bytesread] = '\0';
-		if (!remember)
+		if(!remember)
 			remember = ft_strdup(buffer);
 		else
 			remember = ft_strjoin(remember, buffer);
-		if (ft_strchr(remember, '\n'))
-		{
-			line = fill_line(remember);
-			remember = handle_remember(remember, line);
-		}
+	}
+	return (remember);
+}
+static char	*first_time_read(char *remember, int fd)
+{
+	int	bytesread;
+	char	buffer[BUFFER_SIZE + 1];
+	
+	bytesread = read(fd, buffer, BUFFER_SIZE);
+	if (bytesread < 0)
+		return (NULL);
+	buffer[bytesread] = '\0';
+	remember = ft_strdup(buffer);
+	return (remember);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*remember;
+	char		*line;
+	char		buffer[BUFFER_SIZE + 1];
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!remember)
+		remember = first_time_read(remember, fd);
+	if (ft_strchr(remember, '\n') || !fill_remember(remember, fd))
+	{
+		line = fill_line(remember);
+		if (!line)
+			return (NULL);
+		remember = handle_remember(remember, line);
 		return (line);
 	}
+	else if (!ft_strchr(remember, '\n'))
+	{
+		remember = fill_remember(remember, fd);
+		line = fill_line(remember);
+		remember = handle_remember(remember, line);
+		return (line);
+	}	
 	return (NULL);
 }
 
@@ -75,16 +105,12 @@ int	main()
 {
 	int	fd = open("text.txt", O_RDONLY);
 	
-	puts(get_next_line(fd));
-	puts(get_next_line(fd));
-	puts(get_next_line(fd));
-	puts(get_next_line(fd));
-	puts(get_next_line(fd));
-	// puts(get_next_line(fd));
-	// puts(get_next_line(fd));
-	// puts(get_next_line(fd));
-	// puts(get_next_line(fd));
-	// puts(get_next_line(fd));
-	// puts(get_next_line(fd));
+	char *line;
+
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
 	return 0;
 }
