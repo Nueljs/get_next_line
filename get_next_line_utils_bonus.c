6b351eb5a6db0450b745cb91d4b396f6f442b31f
Manuel Cervera Lara <macerver@c1r2s2.42malaga.com>
2025-12-22 14:14:11 +0100

    bonus

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macerver <macerver@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 05:14:23 by macerver          #+#    #+#             */
+/*   Updated: 2025/12/22 14:08:02 by macerver         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_s;
+	int		i;
+	int		j;
+	int		s1_len;
+	int		s2_len;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new_s = (char *)malloc(s1_len + s2_len + 1);
+	if (!new_s)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		new_s[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < s2_len)
+	{
+		new_s[i + j] = s2[j];
+		j++;
+	}
+	new_s[i + j] = '\0';
+	return (new_s);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char) c)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if (s[i] == (char) c)
+		return ((char *) &s[i]);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		len;
+	int		i;
+	char	*s_copy;
+
+	len = ft_strlen(s);
+	i = 0;
+	s_copy = (char *)malloc((len + 1) * sizeof(char));
+	if (!s_copy)
+		return (NULL);
+	while (i < len)
+	{
+		s_copy[i] = s[i];
+		i++;
+	}
+	s_copy[i] = '\0';
+	return (s_copy);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*dest;
+	size_t	i;
+	size_t	s_len;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	dest = malloc((len + 1) * sizeof(char));
+	if (!dest)
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = s[start + i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
